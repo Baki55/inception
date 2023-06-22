37ed4a2a804e6a9e7050eb6bcc02c05096c61954
baki <badrkhatib@hotmail.com>
2023-06-22 16:54:06 +0200

    no longer need config.sh to gen cert and key (Dockerfile)

--- srcs/requirements/nginx/tools/config.sh
+++
@@ -1,5 +0,0 @@
-mkdir /etc/nginx/ssl
-openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes\
-        -out /etc/nginx/ssl/inception.crt\
-        -keyout /etc/nginx/ssl/inception.key\
-        -subj "/C=BE/ST=BX/L=Bruxelles/O=42/OU=42/CN=bkhatib/"
